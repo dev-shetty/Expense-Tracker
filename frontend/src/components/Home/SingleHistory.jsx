b44f26aef982e13c19e6105f61828080a4b2bf08
Deveesh Shetty <deveeshshetty@gmail.com>
2022-12-07 21:10:40 +0530

    Added final CSS touches

--- frontend/src/components/Home/SingleHistory.jsx
+++ frontend/src/components/Home/SingleHistory.jsx
@@ -5,8 +5,18 @@ function SingleHistory({ singleHistory }) {
       <div className="px-4 py-2">
         <p>{source}</p>
         <div className="flex items-center">
-          <p>{expense > earning ? "-" : "+"}</p>
-          <p className="pl-0.5 font-semibold">
+          <p
+            className={`${
+              expense > earning ? "text-red-500" : "text-green-500"
+            }`}
+          >
+            {expense > earning ? "-" : "+"}
+          </p>
+          <p
+            className={`pl-0.5 font-semibold ${
+              expense > earning ? "text-red-500" : "text-green-500"
+            }`}
+          >
             &#8377;{Math.abs(expense - earning)}
           </p>
         </div>
